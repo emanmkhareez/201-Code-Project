10d303b7f130ebb7fe2da81180af88d9ff3bd1df
emanmkhareez <emkhareez19@gmail.com>
2021-03-29 16:42:00 -0700

    add

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# 201-Code-Project
+#  hospital management system.
+## The purpose of the project called "Hospital Management System" is to computerize hospital front office management to develop an easy-to-use, simple, fast and cost-effective program. It deals with collecting patient information, diagnostic details, etc.
+
+### Author:eman almkhareez &lujain alabed.
+
+ # link and resources:
+ https://www.w3schools.com/
+ https://www.php.net/
---
+++ js/app.js
@@ -0,0 +1,81 @@
+'use strict'
+
+
+let age = prompt("my age 22 ?").toLowerCase()
+
+if (age=='yes'||age=='y') {
+    alert("your answer is correct  = " + age.toLowerCase()+" my age =22")
+
+    console.log("your answer is correct  = " + age.toLowerCase()+" my age =22");
+
+
+} 
+ if (age == 'no' || age == 'n') {
+    alert("your answer is wrong   = " + age.toLowerCase()+" my age=22" )
+
+    console.log("your answer is wrong   = " + age.toLowerCase()+" my age=22" );
+
+} 
+let favoriteDish = prompt("my favorite dish is mansaf ?").toLowerCase();
+
+ if (favoriteDish == 'yes' || favoriteDish == 'y') {
+
+    alert("your answer is wrong   " + favoriteDish.toLowerCase()+" my favorite Dish=dawali ")
+
+    console.log("your answer is wrong   " + favoriteDish.toLowerCase()+" my favorite Dish=dawali ");
+
+}
+
+
+ if (favoriteDish== 'no' || favoriteDish == 'n') {
+  alert("your answer is correct  = " + favoriteDish.toLowerCase()+" my favorite Dish=dawali ")
+
+    console.log("your answer is correct  = " + favoriteDish.toLowerCase()+" my favorite Dish=dawali ");
+
+
+}
+  let color = prompt("my favorite color is red  ?").toLowerCase();
+if (color == 'no' || color == 'n') {
+    alert("your answer is correct  = " + color.toLowerCase()+" my favorite color is black ")
+
+    console.log("your answer is correct  = " + color.toLowerCase()+" my favorite color is black ");
+
+} if (color == 'yes' || color == 'y'){
+    alert("your answer is wrong   " + color.toLowerCase()+ "  my favorite color is black ")
+    console.log("your answer is wrong   " + color.toLowerCase()+ "  my favorite color is black ");
+
+}
+let allergic = prompt("Do I have a spring allergy?").toLowerCase();
+
+ if (allergic == 'yes' || allergic == 'y') {
+    alert("your answer is correct  = " + allergic.toLowerCase()+" i have allergic ")
+
+    console.log("your answer is correct  = " + allergic.toLowerCase()+" i have allergic ");}
+
+ if(allergic == 'no'  || allergic == 'n'){
+
+    alert("your answer is wrong   = " + allergic.toLowerCase()+" i have allergic ")
+    console.log("your answer is wrong   = " + allergic.toLowerCase()+" i have allergic ");}
+
+
+ let home = prompt("Do I like staying home?").toLowerCase();
+
+
+ if(home=='yes' ||home=='y'){
+    alert("your answer is correct  = " + home.toLowerCase()+"I like to stay at home ")
+
+    console.log("your answer is correct  = " + home.toLowerCase()+"I like to stay at home ");
+    alert("Thank you for attempt")
+
+
+    
+} if(home=='no'||home=='n'){
+    alert("your answer is wrong  = " + home.toLowerCase()+"I like to stay at home")
+
+    console.log("your answer is wrong  = " + home.toLowerCase()+"I like to stay at home");
+
+}
+
+
+
+
